09cc8c66afcd3d60e6de99afcf4c3d44323c4eac
gilldunne <x00094469@ittd.ie>
2015-03-04 18:07:06 +0000

    Working thus far

--- DashBoardIBM/db_wrapper.py
+++ DashBoardIBM/db_wrapper.py
@@ -29,20 +29,21 @@ class DBWrapper:
     def get_build_job_collection(self):
         c = self.db.buildjob
         list_of_teamName = self.get_team_names()
-        list_of_buildjobs = []
+        team_list = []
         for team in list_of_teamName:
+            list_of_buildjobs = {}
             query = c.find({"teamName":str(team)}).distinct("name")
-            list_of_buildjobs.append(team)
             length = len(query)
-            list_of_buildjobs.append(length)
-        return list_of_buildjobs
+            list_of_buildjobs.update({"name": team, "buildjob":length})
+            team_list.append(list_of_buildjobs)
+        return team_list
 
 
     # Use build_job collection from TestEasyJLV
     def get_team_names(self):
         c = self.db.buildjob
-        list_of_teamm_names = c.find().distinct("teamName")
-        return list_of_teamm_names
+        list_of_team_names = c.find().distinct("teamName")
+        return list_of_team_names
 
     # Get the user and the servers they are connected to
     def get_user_data(self):
